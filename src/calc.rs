a680272237cf4fda9810a0d6d42f688d4e95858f
Robert <rhorrace95@gmail.com>
2021-05-13 22:08:49 -0600

    Added unit tests

--- src/calc.rs
+++ src/calc.rs
@@ -2,6 +2,7 @@ use crate::card::Card;
 use crate::calc::Rank::*;
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq, PartialOrd)]
 pub enum Rank {
     HighCard,
     OnePair,
@@ -15,72 +16,67 @@ pub enum Rank {
     RoyalFlush,
 }
 
-pub fn highest_rank(hand: &[Card]) -> Rank {
-    let mut suit_count: HashMap<char, u8> = HashMap::new();
-    let mut values: Vec<u8> = Vec::new();
-    if is_flush(&hand, &mut suit_count) {
-        let max_suit = *suit_count.iter()
-            .max_by_key(|entry| entry.1)
-            .unwrap().0;
-        let flush: Vec<Card> = hand.iter()
-            .filter(|&c| c.1 == max_suit)
-            .map(|c| c.clone())
-            .collect();
-        if is_straight(&flush, &mut values) {
-            if values[0] == 14 && values[4] == 10 {
-                RoyalFlush
-            } else {
-                StraightFlush
-            }
-        } else {
-            Flush
-        }
-    } else if is_straight(&hand, &mut values) {
+pub fn calc_rank(hand: &[Card]) -> Rank {
+    let mut cards: Vec<Card> = hand.to_vec();
+    cards.sort();
+    let mut uniq_cards: Vec<Card> = cards.clone();
+    uniq_cards.dedup_by(|a, b| a == b);
+    if let Some(rank) = check_flush(&cards) {
+        rank
+    } else if is_straight(&uniq_cards) {
         Straight
     } else {
-        HighCard
+        other_rank(&cards)
     }
 }
 
-fn is_flush(hand: &[Card], suit_count: &mut HashMap<char, u8>) -> bool {
+fn check_flush(hand: &[Card]) -> Option<Rank> {
+    let mut groups: HashMap<char, Vec<Card>> = "HDSC".chars().map(|c| (c, Vec::new())).collect();
     for card in hand {
-        *suit_count.entry(card.1).or_insert(0) += 1;
+        groups.entry(card.1)
+            .or_insert(Vec::new())
+            .push(*card);
     }
-    *suit_count.values()
-        .max()
-        .unwrap() >= 5
-}
-
-fn is_straight(hand: &[Card], values: &mut Vec<u8>) -> bool {
-    let mut card_values: Vec<u8> = hand.iter()
-        .map(|c| c.2)
-        .collect();
-    card_values.sort();
-    if card_values.last() == Some(&14) {
-        values.push(1);
-    }
-    values.append(&mut card_values);
-    values.dedup();
-    values.reverse();
-    let mut count = 1;
-    let mut prev = values[0];
-    for &i in values.iter().skip(1) {
-        if i == prev - 1 {
-            count += 1;
-            if count == 5 {
-                return true;
-            }
-        } else {
-            count = 1;
+    let mut flush: Vec<Card> = Vec::new();
+    for cards in groups.values() {
+        if cards.len() >= 5 {
+            flush = cards.clone();
+            break;
         }
-        prev = i;
     }
-    false
+    if flush.is_empty() {
+        None
+    } else if is_straight(&flush) {
+        flush.reverse();
+        if flush[0].2 == 14 && flush[4].2 == 10 {
+            Some(RoyalFlush)
+        } else {
+            Some(StraightFlush)
+        }
+    } else {
+        Some(Flush)
+    }
 }
 
-fn is_other(hand: &[Card]) -> Rank {
-    let mut value_count: HashMap<u8,u8> = HashMap::new();
-    let mut counts: HashMap<u8,u8> = (1..=4).map(|i| (i, 0)).collect::<HashMap<_,_>>();
+fn is_straight(hand: &[Card]) -> bool {
+    let mut cards = hand.to_vec();
+    if let Some(&card) = cards.last() {
+        if card.0 == 'A' {
+            let mut ace_low: Card = card;
+            ace_low.2 = 1;
+            cards.insert(0, ace_low);
+        }
+    }
+    match hand.len() >= 5 {
+        false => false,
+        true => hand.windows(5)
+            .any(|straight| straight[4].2 - straight[0].2 == 4)
+    }
+}
+
+fn other_rank(hand: &[Card]) -> Rank {
+    let mut value_count: HashMap<u8, u8> = HashMap::new();
+    let mut counts: HashMap<u8, u8> = (1..=4).map(|i| (i, 0)).collect::<HashMap<_, _>>();
     for card in hand {
         *value_count.entry(card.2).or_insert(0) += 1;
     }
@@ -101,5 +97,79 @@ fn is_other(hand: &[Card]) -> Rank {
         OnePair
     } else {
         HighCard
+    }
+}
+
+#[cfg(test)]
+mod calc_tests {
+    use crate::card::Card;
+    use crate::calc::calc_rank;
+    use crate::calc::Rank::*;
+
+    #[test]
+    fn test_rank() {
+        assert!(RoyalFlush > Straight);
+    }
+
+    #[test]
+    fn test_royal_flush() {
+        let hand: Vec<Card> = vec![Card('A', 'H', 14), Card('K', 'H', 13), Card('2', 'D', 2), Card('Q', 'H', 12), Card('7', 'S', 7), Card('J', 'H', 11), Card('_', 'H', 10)];
+        assert_eq!(calc_rank(&hand), RoyalFlush)
+    }
+
+    #[test]
+    fn test_straight_flush() {
+        let hand: Vec<Card> = vec![Card('9', 'H', 9), Card('K', 'H', 13), Card('2', 'D', 2), Card('Q', 'H', 12), Card('7', 'S', 7), Card('J', 'H', 11), Card('_', 'H', 10)];
+        assert_eq!(calc_rank(&hand), StraightFlush)
+    }
+
+    #[test]
+    fn test_four_of_kind() {
+        let hand: Vec<Card> = vec![Card('9', 'H', 9), Card('2', 'D', 2), Card('9', 'S', 9), Card('9', 'D', 9), Card('2', 'H', 2), Card('9', 'C', 9), Card('2', 'C', 2)];
+        assert_eq!(calc_rank(&hand), FourOfKind);
+    }
+
+    #[test]
+    fn test_full_house() {
+        let hand1: Vec<Card> = vec![Card('9', 'H', 9), Card('2', 'D', 2), Card('9', 'S', 9), Card('9', 'D', 9), Card('2', 'H', 2), Card('3', 'C', 3), Card('2', 'C', 2)];
+        let hand2: Vec<Card> = vec![Card('3', 'H', 3), Card('2', 'D', 2), Card('9', 'S', 9), Card('9', 'D', 9), Card('2', 'H', 2), Card('3', 'C', 3), Card('2', 'C', 2)];
+        assert_eq!(calc_rank(&hand1), FullHouse);
+        assert_eq!(calc_rank(&hand2), FullHouse);
+    }
+
+    #[test]
+    fn test_flush() {
+        let hand: Vec<Card> = vec![Card('9', 'H', 9), Card('K', 'H', 13), Card('2', 'D', 2), Card('2', 'H', 2), Card('7', 'S', 7), Card('J', 'H', 11), Card('_', 'H', 10)];
+        assert_eq!(calc_rank(&hand), Flush)
+    }
+
+    #[test]
+    fn test_straight() {
+        let hand: Vec<Card> = vec![Card('9', 'D', 9), Card('K', 'C', 13), Card('2', 'D', 2), Card('Q', 'H', 12), Card('7', 'S', 7), Card('J', 'H', 11), Card('_', 'H', 10)];
+        assert_eq!(calc_rank(&hand), Straight)
+    }
+
+    #[test]
+    fn test_three_of_kind() {
+        let hand: Vec<Card> = vec![Card('9', 'H', 9), Card('7', 'D', 7), Card('3', 'S', 3), Card('9', 'D', 9), Card('2', 'H', 2), Card('9', 'C', 9), Card('5', 'C', 5)];
+        assert_eq!(calc_rank(&hand), ThreeOfKind);
+    }
+
+    #[test]
+    fn test_two_pair() {
+        let hand: Vec<Card> = vec![Card('9', 'H', 9), Card('2', 'D', 2), Card('9', 'S', 9), Card('8', 'D', 8), Card('2', 'H', 2), Card('6', 'C', 6), Card('4', 'C', 4)];
+        assert_eq!(calc_rank(&hand), TwoPair);
+    }
+
+    #[test]
+    fn test_one_pair() {
+        let hand: Vec<Card> = vec![Card('A', 'H', 14), Card('2', 'D', 2), Card('9', 'S', 9), Card('8', 'D', 8), Card('2', 'H', 2), Card('6', 'C', 6), Card('4', 'C', 4)];
+        assert_eq!(calc_rank(&hand), OnePair);
+    }
+
+    #[test]
+    fn test_high_card() {
+        let hand: Vec<Card> = vec![Card('A', 'H', 14), Card('J', 'D', 11), Card('9', 'S', 9), Card('8', 'D', 8), Card('2', 'H', 2), Card('6', 'C', 6), Card('4', 'C', 4)];
+        assert_eq!(calc_rank(&hand), HighCard);
     }
 }
--- src/card.rs
+++ src/card.rs
@@ -1,20 +1,149 @@
 use std::fmt::{Display, Formatter};
+use std::cmp::{Ordering};
 
-#[derive(Clone)]
-pub struct Card(pub &'static str, pub char, pub u8);
-
-impl Card {
-    pub fn new(name: &'static str, suit: char, value: u8) -> Card {
-        Card(name, suit, value)
-    }
-
-    pub fn is(self, name: &str) -> bool {
-        self.0.eq(name)
-    }
-}
+#[derive(Clone, Copy)]
+pub struct Card(pub char, pub char, pub u8);
 
 impl Display for Card {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}:{}", self.0, self.1)
+        let name_str = match self.2 {
+            10 => self.2.to_string(),
+            _ => self.0.to_string()
+        };
+        write!(f, "{}:{}", name_str, self.1)
+    }
+}
+
+impl Eq for Card {}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.2.cmp(&other.2)
+    }
+}
+
+impl PartialEq for Card {
+    fn eq(&self, other: &Self) -> bool {
+        self.2 == other.2
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.2 != other.2
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.2.partial_cmp(&other.2)
+    }
+
+    fn lt(&self, other: &Self) -> bool {
+        self.2 < other.2
+    }
+
+    fn le(&self, other: &Self) -> bool {
+        self.2 <= other.2
+    }
+
+    fn gt(&self, other: &Self) -> bool {
+        self.2 > other.2
+    }
+
+    fn ge(&self, other: &Self) -> bool {
+        self.2 >= other.2
+    }
+}
+
+#[cfg(test)]
+mod card_tests {
+    use crate::card::Card;
+    use std::cmp::Ordering;
+
+    #[test]
+    fn test_new() {
+        let card: Card = Card('A', 'H', 14);
+        assert_eq!(card.0, 'A');
+        assert_eq!(card.1, 'H');
+        assert_eq!(card.2, 14);
+    }
+
+    #[test]
+    fn test_cmp() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('5', 'D', 5);
+        let card4: Card = Card('A', 'D', 14);
+        assert_eq!(card1.cmp(&card2), Ordering::Equal);
+        assert_eq!(card1.cmp(&card3), Ordering::Greater);
+        assert_eq!(card1.cmp(&card4), Ordering::Less);
+    }
+
+    #[test]
+    fn test_eq() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('5', 'D', 5);
+        assert!(card1 == card2);
+        assert_eq!(card1 == card3, false);
+    }
+
+    #[test]
+    fn test_ne() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('5', 'D', 5);
+        assert_eq!(card1 != card2, false);
+        assert!(card1 != card3);
+    }
+
+    #[test]
+    fn test_partial_cmp() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('5', 'D', 5);
+        let card4: Card = Card('A', 'D', 14);
+        assert_eq!(card1.partial_cmp(&card2), Some(Ordering::Equal));
+        assert_eq!(card1.partial_cmp(&card3), Some(Ordering::Greater));
+        assert_eq!(card1.partial_cmp(&card4), Some(Ordering::Less));
+    }
+
+    #[test]
+    fn test_lt() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('J', 'D', 11);
+        let card3: Card = Card('5', 'D', 5);
+        assert!(card1 < card2);
+        assert_eq!(card1 < card3, false);
+    }
+
+    #[test]
+    fn test_le() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('J', 'D', 11);
+        let card4: Card = Card('5', 'D', 5);
+        assert!(card1 <= card2);
+        assert!(card1 <= card3);
+        assert_eq!(card1 <= card4, false);
+    }
+
+    #[test]
+    fn test_gt() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('5', 'D', 5);
+        let card3: Card = Card('J', 'D', 11);
+        assert!(card1 > card2);
+        assert_eq!(card1 > card3, false);
+    }
+
+    #[test]
+    fn test_ge() {
+        let card1: Card = Card('_', 'H', 10);
+        let card2: Card = Card('_', 'D', 10);
+        let card3: Card = Card('5', 'D', 5);
+        let card4: Card = Card('J', 'D', 11);
+        assert!(card1 >= card2);
+        assert!(card1 >= card3);
+        assert_eq!(card1 >= card4, false);
     }
 }
