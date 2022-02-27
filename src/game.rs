ca6efb75f65a2bc8534db6bfe5944fde6885980f
Robert <rhorrace95@gmail.com>
2022-02-27 15:17:22 -0700

    Added Texas Holdem

--- src/game.rs
+++ src/game.rs
@@ -4,11 +4,37 @@ use crate::calc::{calc_rank, Rank, calc_best_hand};
 use crate::card::{Card, Name, Name::*, Suit, Suit::*};
 use rand::{seq::SliceRandom, thread_rng};
 use std::collections::VecDeque;
+use std::io;
+use std::io::Write;
+use crate::game::Phase::*;
+
+pub fn read_user(output: &str) -> i8 {
+    print!("{}", output);
+    io::stdout().flush().expect("Error: Flush failed");
+    let mut input = String::new();
+    let mut response = -1;
+    io::stdin().read_line(&mut input).expect("Error: Read failed");
+    if let Ok(num) = input.trim().parse::<i8>() {
+        response = num;
+    }
+    response
+}
+
+pub enum Phase {
+    Clean,
+    DealStud,
+    DealTexas,
+    Flop,
+    Turn,
+    River,
+    Winner,
+}
 
 pub struct Game {
     deck: VecDeque<Card>,
     player: Vec<Card>,
     computer: Vec<Card>,
+    community: Vec<Card>,
     //burned: Vec<Card>
 }
 
@@ -21,6 +47,7 @@ impl Game {
             deck: build_deck(),
             player: Vec::new(),
             computer: Vec::new(),
+            community: Vec::new(),
             //burned: Vec::new()
         }
     }
@@ -28,28 +55,103 @@ impl Game {
     // Public functions
 
     pub fn play_seven_card_stud(&mut self) {
-        self.shuffle();
-        self.deal(7);
-        self.display_hands();
-        self.winner();
+        let mut choice: i8;
+        loop {
+            // Clear table if needed
+            self.update_game(Clean);
+
+            // Deal
+            self.update_game(DealStud);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+
+            // Winner
+            self.update_game(Winner);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+        }
     }
 
     pub fn play_texas_holdem(&mut self) {
+        let mut choice: i8;
+        loop {
+            // Clear table if needed
+            self.update_game(Clean);
 
+            // Deal
+            self.update_game(DealTexas);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+
+            // Flop
+            self.update_game(Flop);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+
+            // Turn
+            self.update_game(Turn);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+
+            // River
+            self.update_game(River);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+
+            // Winner
+            self.update_game(Winner);
+            choice = read_user("Enter) Continue 1) Quit: ");
+            if choice == 1 {
+                break;
+            }
+        }
     }
 
     // Private functions
 
-    fn deal(&mut self, n: u8) {
-        for _ in 0..n {
-            self.player.push(self.deck.pop_front().unwrap());
-            self.computer.push(self.deck.pop_front().unwrap());
+    fn clean(&mut self) {
+        if !self.community.is_empty() {
+            self.deck.extend(&self.community);
+            self.community.clear();
+        }
+        if !self.player.is_empty() {
+            self.deck.extend(&self.player);
+            self.player.clear();
+        }
+        if !self.computer.is_empty() {
+            self.deck.extend(&self.computer);
+            self.computer.clear();
         }
     }
 
-    fn display_hands(&self) {
+    fn deal(&mut self, n: usize) {
+        let cards: Vec<Card> = self.deck.drain(..(n * 2)).collect();
+        for pair in cards.chunks(2) {
+            self.player.push(pair[0]);
+            self.computer.push(pair[1]);
+        }
+    }
+
+    fn display_table(&self, show_computer: bool) {
         println!("Player:   {:?}", self.player);
-        println!("Computer: {:?}", self.computer);
+
+        if show_computer {
+            println!("Computer: {:?}", self.computer);
+        }
+
+        println!("Community: {:?}", self.community);
     }
 
     fn shuffle(&mut self) {
@@ -60,15 +162,54 @@ impl Game {
         self.deck = VecDeque::from(deck_vec);
     }
 
+    fn update_community(&mut self, n: usize) {
+        let cards: Vec<Card> = self.deck.drain(..n).collect();
+        self.community.extend(cards);
+    }
+
+    fn update_game(&mut self, phase: Phase) {
+        print!("\x1B[2J\x1B[1;1H");
+        match phase {
+            Clean => {
+                self.clean();
+                self.shuffle();
+            }
+            DealStud => {
+                self.deal(7);
+                self.display_table(false);
+            }
+            DealTexas => {
+                self.deal(2);
+                self.display_table(false);
+            }
+            Flop => {
+                self.update_community(3);
+                self.display_table(false);
+            }
+            Turn | River => {
+                self.update_community(1);
+                self.display_table(false);
+            }
+            Winner => {
+                self.display_table(true);
+                self.winner();
+            }
+        }
+    }
+
     fn winner(&self) {
-        let player_rank: Rank = calc_rank(&self.player);
-        let computer_rank: Rank = calc_rank(&self.computer);
-        let player_best_hand: Vec<Card> = calc_best_hand(&self.player, player_rank);
-        let computer_best_hand: Vec<Card> = calc_best_hand(&self.computer, computer_rank);
-        println!("Player:   {}", player_rank);
-        println!("\t{:?}", player_best_hand);
-        println!("Computer: {}", computer_rank);
-        println!("\t{:?}", computer_best_hand);
+        let mut player_hand = self.player.clone();
+        let mut computer_hand = self.computer.clone();
+
+        player_hand.extend(&self.community);
+        computer_hand.extend(&self.community);
+
+        let player_rank: Rank = calc_rank(&player_hand);
+        let computer_rank: Rank = calc_rank(&computer_hand);
+
+        let player_best_hand: Vec<Card> = calc_best_hand(&player_hand, player_rank);
+        let computer_best_hand: Vec<Card> = calc_best_hand(&computer_hand, computer_rank);
+
         if player_rank > computer_rank {
             println!("You Win!");
         } else if player_rank < computer_rank {
@@ -76,6 +217,11 @@ impl Game {
         } else {
             self.tie_breaker(&player_best_hand, &computer_best_hand);
         }
+
+        println!("Player:   {}", player_rank);
+        println!("\t{:?}", player_best_hand);
+        println!("Computer: {}", computer_rank);
+        println!("\t{:?}", computer_best_hand);
     }
 
     fn tie_breaker(&self, player: &[Card], computer: &[Card]) {
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ mod calc;
 mod card;
 mod game;
 
-use crate::game::Game;
+use crate::game::{Game, read_user};
 
 fn main() {
     let mut games: Game = Game::new();
-    games.play_seven_card_stud();
+    loop {
+        let choice: i8 = read_user("1) 7 Card Stud 2) Texas Hold'em 0) Quit: ");
+        if choice == 0 {
+            break;
+        }
+        match choice {
+            1 => games.play_seven_card_stud(),
+            2 => games.play_texas_holdem(),
+            _ => {}
+        }
+    }
+    println!("Goodbye!");
 }
